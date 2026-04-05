da52b6df6f46ee2178162f213253b3bb5462c1e4
Gyo <mbentogyo@gmail.com>
2026-04-05 20:13:37 +0800

    chore: added .env

--- src/main/java/dev/gracco/db/Database.java
+++ src/main/java/dev/gracco/db/Database.java
@@ -1,11 +1,26 @@
 package dev.gracco.db;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Database {
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/INSERT_NAME";
-    private static final String DB_USER = "INSERT_USER";
-    private static final String DB_PASSWORD = "INSERT_PASSWORD";
+    private static Connection connection;
 
     public static boolean initialize() {
+        Dotenv dotenv = Dotenv.load();
+        String DB_URL = dotenv.get("DB_URL");
+        String DB_USER = dotenv.get("DB_USER");
+        String DB_PASSWORD = dotenv.get("DB_PASSWORD");
+
+        try {
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        } catch (SQLException e) {
+            //todo: do something
+        }
+
         return true;
     }
 }
